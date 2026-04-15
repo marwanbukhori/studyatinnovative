8491893e02481405f7a82a65bd7163be00e281d2
Marwan Bukhori <mastamaru@gmail.com>
2026-04-15 10:59:25 +0800

    feat: integrate Vercel Analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
+import { inject } from "@vercel/analytics";
 import App from "./App.vue";
 import i18n from "./i18n";
 import "./styles/main.css";
 import "./utils/tracking";
+
+inject();
 
 createApp(App).use(i18n).mount("#app");
